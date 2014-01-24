3f544f901d9492536c0fb1796da0521c2a1e9b03
Bruno Paulino <brunojppb@gmail.com>
2014-01-23 23:02:47 -0300

    primeiro commit

---
+++ sistema/escalas/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+from sistema.escalas.views import *
+
+urlpatterns = patterns('',
+	url(r'^guarnicao/listar/$', guarnicao_listar),
+    url(r'^guarnicao/criar/$', guarnicao_criar),
+    url(r'^guarnicao/(?P<ident>\d)/$', guarnicao_detalhes),
+    url(r'^militar/(?P<mat>\d{7})/$', militar_detalhes),
+    url(r'^militar/editar/(?P<ident>\d)/$', militar_editar),
+    url(r'^militar/criar/$', militar_criar),
+    url(r'^militar/listar/$', militar_listar),
+    url(r'^militar/teste/$', teste),
+    url(r'^militar/ajax/$', ajax),
+
+)
